847d79fa4eef1c4f04c8087791b2ab15312d460e
Juan David Casallas Pava <davidcasallas1202@gmail.com>
2024-08-28 14:00:44 -0500

    Taller Javascript

---
+++ Taller_Javascript/scripts/ej3Basico.js
@@ -0,0 +1,5 @@
+let base = prompt("Introduce la base del triángulo");
+let altura = prompt("Introduce la altura del triángulo");
+let area = (base * altura) 
+let perimetro = 2*(base + altura);
+window.alert(`El area del rectangulo es de: ${area} con un perimetro de: ${perimetro}`);
